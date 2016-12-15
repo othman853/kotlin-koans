9425e5399d9f948b7f2c72afb5f61a15743356f3
othman853 <anuar.yasser@gmail.com>
2016-12-15 18:03:44 -0200

    Solving exercise 4

--- src/i_introduction/_4_Lambdas/Lambdas.kt
+++ src/i_introduction/_4_Lambdas/Lambdas.kt
@@ -21,7 +21,13 @@ fun todoTask4(collection: Collection<Int>): Nothing = TODO(
     documentation = doc4(),
     references = { JavaCode4().task4(collection) })
 
-fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
+fun task4(collection: Collection<Int>): Boolean {
+
+    val t4 = { x: Int -> x % 42 == 0 }
+
+    return collection.any(t4)
+
+}
 
 
 
